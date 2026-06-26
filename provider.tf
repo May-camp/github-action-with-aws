d37cffdf8aead5683037a2aaa6cd230179e1dfd7
May Yadanar Maung / Cloud and DevOps skill set <nadihlaing776655@gmail.com>
2026-06-26 20:08:04 +0630

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -13,4 +13,15 @@ provider "aws" {
   region                      = "us-east-1"
   access_key                  = "mock_key"
   secret_key                  = "mock_secret"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  
+  s3_use_path_style           = true 
+
+  endpoints {
+    s3  = "http://127.0.0.1:4566"
+    kms = "http://127.0.0.1:4566"
+    sts = "http://127.0.0.1:4566"  # <-- This fixes the STS 403 error!
+  }
 }
