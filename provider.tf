9af99eeca4bff789fee5a1d80c40f42e48669122
May-camp <nadihlaing776655@gmail.com>
2026-06-26 11:50:36 +0000

    ci cd acton added

--- provider.tf
+++ provider.tf
@@ -8,5 +8,15 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region                      = "us-east-1"
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3  = "http://localhost:4566"
+    kms = "http://localhost:4566"
+    # Add any other AWS services you are using here
+  }
 }
