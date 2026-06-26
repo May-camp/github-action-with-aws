fa36bf0765e58ab47f44af865799df43523c8e37
May Yadanar Maung / Cloud and DevOps skill set <nadihlaing776655@gmail.com>
2026-06-26 20:05:26 +0630

    Clean up AWS provider configuration
    Removed unused provider configuration options.

--- provider.tf
+++ provider.tf
@@ -13,12 +13,4 @@ provider "aws" {
   region                      = "us-east-1"
   access_key                  = "mock_key"
   secret_key                  = "mock_secret"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-
-  endpoints {
-   s3  = "http://localstack:4566"
-    kms = "http://localstack:4566"
-  }
 }
